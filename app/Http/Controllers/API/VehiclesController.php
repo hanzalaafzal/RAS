8b528d61d145b98581404ce8b64044fe252b212a
malikhunzala1337@gmail.com <48134105+hanzalaafzal@users.noreply.github.com>
2021-10-13 15:34:44 +0500

    API

--- app/Http/Controllers/API/VehiclesController.php
+++ app/Http/Controllers/API/VehiclesController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
 use DB;
+use App\Vehicles;
 
 class VehiclesController extends Controller
 {
 
     public function getSingleVehicle($vcode){
+      $data=Vehicles::where('vcode',$vcode)->get()->toArray();
       $data=DB::table('vehicles')->where('vcode' ,$vcode)->get()->toArray();
       $data[0]->qr_url=route('verifyvehicle').'?method=false&verficationCode='.$data[0]->vcode;
       return response(json_encode($data),200);
