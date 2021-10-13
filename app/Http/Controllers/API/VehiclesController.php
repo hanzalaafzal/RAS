e947a3a7514a22adb26699a90c351e04948b48a6
malikhunzala1337@gmail.com <48134105+hanzalaafzal@users.noreply.github.com>
2021-10-13 14:34:10 +0500

    QR URL

--- app/Http/Controllers/API/VehiclesController.php
+++ app/Http/Controllers/API/VehiclesController.php
@@ -12,6 +12,7 @@ class VehiclesController extends Controller
 
     public function getSingleVehicle($vcode){
       $data=DB::table('vehicles')->where('vcode' ,$vcode)->get()->toArray();
+      $data[0]->qr_url=route('verifyvehicle').'?method=false&verficationCode='.$data[0]->vcode;
       return response(json_encode($data),200);
     }
 
